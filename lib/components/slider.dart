3ac85e82eb488f1ee10ebc4c54a8fde81ed88086
GregSym <gtsrex@gmail.com>
2021-07-17 04:51:42 +0200

    rebuilt slider component to use a state provider

--- lib/components/slider.dart
+++ lib/components/slider.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_podcast_app/controllers/podcast_player_controller.dart';
+import 'package:provider/provider.dart';
 
 class PodcastSlider extends StatelessWidget {
   const PodcastSlider({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Slider(value: 2, onChanged: null);
+    bool _isInTransition = false;
+    double _directValue = 0.0;
+    return Consumer<PodcastPlayerController>(
+        builder: (context, _podcastPlayerController, _) =>
+            _podcastPlayerController.isInitialized
+                ? Slider(
+                    activeColor: Colors.red,
+                    value: (_isInTransition)
+                        ? _directValue
+                        : _podcastPlayerController.position,
+                    max: _podcastPlayerController.duration,
+                    onChanged: (value) {
+                      _isInTransition = true;
+                      _directValue = value;
+                      _podcastPlayerController
+                          .seekTo(value)
+                          .then((value) => _isInTransition = false);
+                    })
+                : CircularProgressIndicator());
   }
 }
--- lib/controllers/podcast_player_controller.dart
+++ lib/controllers/podcast_player_controller.dart
@@ -8,6 +8,21 @@ class PodcastPlayerController {
   /// controllers for different platforms
   BetterPlayerController get podcastController => _podcastController;
 
+  bool get isInitialized {
+    if (_podcastController.isVideoInitialized() == null) {
+      return false;
+    }
+    return _podcastController.isVideoInitialized()!;
+  }
+
+  double get position =>
+      _podcastController.videoPlayerController!.value.position.inMilliseconds
+          .toDouble(); // TODO: add a null check here
+
+  double get duration =>
+      _podcastController.videoPlayerController!.value.duration!.inMilliseconds
+          .toDouble(); // TODO: add a null check here
+
   get events => null;
 
   // METHODS
@@ -15,6 +30,9 @@ class PodcastPlayerController {
   play() => null;
 
   pause() => null;
+
+  Future<void> seekTo(double position) =>
+      _podcastController.seekTo(Duration(milliseconds: (position).toInt()));
 
   skipForward() => null;
 
