79e405b264637d5cc7acb2e1801e0871c5976c16
GregSym <gtsrex@gmail.com>
2021-07-19 00:17:38 +0200

    added the first screen body

---
+++ lib/screens/podcast_feed.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PodcastFeed extends StatelessWidget {
+  const PodcastFeed({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: ListView(
+          children: [],
+        ),
+      ),
+    ));
+  }
+}
