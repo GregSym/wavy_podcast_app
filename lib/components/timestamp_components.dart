437abfb88a9a8a6a47ec631c79cd099eaa7ee096
GregSym <gtsrex@gmail.com>
2021-07-18 22:54:10 +0200

    added controller for the podcast rss feeds

---
+++ lib/components/timestamp_components.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_podcast_app/controllers/podcast_player_controller.dart';
+import 'package:provider/provider.dart';
+
+/// shows both position and duration by default
+/// - setting this to false returns
+/// position by default
+/// - set justDuration to true to get just the duration - overrides positionAndDuration
+/// flag
+class PodcastTimestamp extends StatelessWidget {
+  final bool positionAndDuration;
+  final bool justDuration;
+  const PodcastTimestamp({
+    Key? key,
+    this.positionAndDuration = true,
+    this.justDuration = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PodcastPlayerController>(
+      builder: (context, _podcastController, _) => justDuration
+          ? Text(
+              "${_podcastController.duration}",
+            )
+          : !positionAndDuration
+              ? Text(
+                  "${_podcastController.position} / ${_podcastController.duration}",
+                )
+              : Text(
+                  "${_podcastController.position}",
+                ),
+    );
+  }
+}
---
+++ lib/controllers/podcast_stream.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/foundation.dart';
+import 'package:webfeed/webfeed.dart';
+import 'package:http/http.dart' as http;
+
+class Podcast with ChangeNotifier {
+  late RssFeed _feed;
+  late RssItem _selectedItem;
+  late Map<String, bool> downloadStatus;
+  final String url =
+      'https://feeds.simplecast.com/wjQvYtdl'; //mbmbam probably exists, right?
+
+  RssFeed get feed => _feed;
+  void parse() async {
+    final res = await http.get(Uri.parse(
+        url)); // remember to parse the url string because they made the package worse?
+    final strXml = res.body;
+    _feed = RssFeed.parse(strXml);
+    notifyListeners();
+  }
+
+  RssItem get selectedItem => _selectedItem;
+  set selectedItem(RssItem value) {
+    _selectedItem = value;
+    notifyListeners();
+  }
+
+  /*
+  void download(RssItem item) async {
+    final client = http.Client();
+    final req = http.Request("GET", Uri.parse(item.enclosure.url));
+    final res = await client.send(req);
+    if (res.statusCode != 200) {
+      throw Exception('Unexpected HTTP code: ${res.statusCode}');
+    }
+    res.stream.listen((bytes) {
+      print("Received ${bytes.length} bytes");
+    });
+    final file = File(await _getDownloadDirectory());
+    res.stream
+        .pipe(file.openWrite())
+        .whenComplete(() => print('Downloading complete'));
+  }
+  */
+}
