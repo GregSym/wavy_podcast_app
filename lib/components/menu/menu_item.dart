b8807d305f3ea4759f9807132478186a0b088198
GregSym <gtsrex@gmail.com>
2021-07-19 13:56:22 +0200

    added new menu item component

---
+++ lib/components/menu/menu_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_podcast_app/controllers/podcast_player_controller.dart';
+import 'package:flutter_podcast_app/controllers/podcast_stream.dart';
+import 'package:provider/provider.dart';
+import 'package:webfeed/domain/rss_item.dart';
+
+class PodcastMenuItem extends StatelessWidget {
+  final RssItem rssItem;
+  const PodcastMenuItem({Key? key, required this.rssItem}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String _fallbackImage = "";
+    return ListTile(
+      leading: Image.network(rssItem.itunes!.image!.href ?? _fallbackImage),
+      title: Text(rssItem.title ?? "Missing title info"),
+      subtitle: Text(rssItem.author ?? "Missing publishing info"),
+      trailing: Icon(Icons.play_arrow_rounded),
+      onTap: () => _handlePodcastSelection(context),
+    );
+  }
+
+  _handlePodcastSelection(BuildContext context) {
+    context.read<Podcast>().selectedItem = rssItem;
+    context.read<PodcastPlayerController>().currentTrack = rssItem;
+    Navigator.of(context).pushNamed("/podcast-player");
+  }
+}
--- lib/screens/podcast_feed.dart
+++ lib/screens/podcast_feed.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_podcast_app/components/menu/menu_item.dart';
+import 'package:flutter_podcast_app/controllers/podcast_stream.dart';
+import 'package:provider/provider.dart';
 
 class PodcastFeed extends StatelessWidget {
   const PodcastFeed({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // handle nullables
+    if (context.read<Podcast>().feed.items == null) {
+      return Text("Unexpected RSS structure, no items present in feed");
+    }
+    // main functionality
     return SafeArea(
         child: Scaffold(
       body: Center(
         child: ListView(
-          children: [],
+          children: context
+              .read<Podcast>()
+              .feed
+              .items!
+              .map((rssItem) => PodcastMenuItem(rssItem: rssItem))
+              .toList(),
         ),
       ),
     ));
