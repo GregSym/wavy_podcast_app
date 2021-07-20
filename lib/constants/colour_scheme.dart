732ea90b786dcddbae82c2bd6e81066482ef5565
GregSym <gtsrex@gmail.com>
2021-07-20 22:25:49 +0200

    added more fields to the constants folder

---
+++ lib/constants/colour_scheme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColourScheme {
+  // TODO: come up with a design you can populate this, ya schmuck
+  static MaterialColor primaryColour = Colors.cyan;
+}
