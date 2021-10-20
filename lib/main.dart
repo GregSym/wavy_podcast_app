b44abc66b5c6e84573b6529a3523e75e120c57ca
GregSym <gtsrex@gmail.com>
2021-10-20 23:48:41 +0200

    commenting out the url strategy in other branches for now

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter_podcast_app/services/color_service.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  Beamer.setPathUrlStrategy();
+  // Beamer.setPathUrlStrategy();  // has some issues in deployment
   runApp(MyApp());
 }
 
