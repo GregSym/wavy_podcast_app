ced72bd1d9dee2472fcfa797fe4ac627d52f3544
GregSym <gtsrex@gmail.com>
2021-11-11 01:49:48 +0200

    more shared_prefs setup

--- lib/services/shared_prefs.dart
+++ lib/services/shared_prefs.dart
@@ -14,9 +14,26 @@ class AbstractDBService {
 /// - NOTE ALSO: this is a bad idea
 /// and you should not copy this practice, I'm being *naughty*
 class SharedPreferencesService {
-  SharedPreferencesService() {}
+  SharedPreferencesService() {
+    createPrefReference();
+  }
+  late SharedPreferences prefs;
+  List<String>? _subscriptions = [];
 
   createPrefReference() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
+  }
+
+  getSubscriptions() async =>
+      _subscriptions = this.prefs.getStringList('subscriptions');
+
+  setSubscriptions(List<String> newSubscriptionList) async =>
+      this.prefs.setStringList('subscriptions', newSubscriptionList);
+
+  addSubscription(String subscriptionUri) async {
+    await this.getSubscriptions(); // latest subscriptions
+    if (this._subscriptions == null) _subscriptions = [];
+    _subscriptions!.add(subscriptionUri);
+    this.prefs.setStringList('subscriptions', _subscriptions!);
   }
 }
