5ea5f622fc6e5ac97b6d9bb3e7a21ae56ebbf832
GregSym <gtsrex@gmail.com>
2021-11-11 02:01:35 +0200

    added user profile class name - currently empty

---
+++ lib/models/user_profile_info.dart
@@ -0,0 +1,7 @@
+/// beginnings of a mock-up for storing User Profile info
+/// preferably this can be as local as possible while still being powerful
+/// obviously persisting across devices requires cloud stuff so it's best to
+/// keep this as small as possible
+class WavyUserProfile {
+  List<String> subscriptions = [];
+}
